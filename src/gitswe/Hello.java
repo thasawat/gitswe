0371912248590cdc1ff4f6db721bc199102af005
DESKTOP-T4K3K5I\Jo_PC <jo_sometime@hotmail.com>
2017-09-14 08:43:08 +0700

    commit 8.43

--- src/gitswe/Hello.java
+++ src/gitswe/Hello.java
@@ -3,12 +3,11 @@ package gitswe;
 public class Hello {
 	
 	public static void main(String[] args) {
+		System.out.println("Hello P' tong");
         System.out.println("Hello Java Github");
         System.out.println("Hello Java Github 2 ");
         System.out.println("Hello Java Github 3 ");
-        
-        System.out.println("Hello Java Github 4 ");
-        
+       
     }
 
 
