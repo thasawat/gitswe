577dc45c7cffddbb644ae079cf9249aa7bc0ed4c
DESKTOP-T4K3K5I\Jo_PC <jo_sometime@hotmail.com>
2017-09-14 09:31:52 +0700

    calc function

--- src/gitswe/Hello.java
+++ src/gitswe/Hello.java
@@ -3,14 +3,21 @@ package gitswe;
 public class Hello {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello P' tong");
-        System.out.println("Hello Java Github");
-        System.out.println("Hello Java Github 3 ");
+		
         
         
-        System.out.println("TESTTT ");
-        System.out.println("COMMIT");
+        System.out.println(calc(5));
+        
     }
-
-
+	private static boolean calc(int i) {
+		// TODO Auto-generated method stub
+		
+		if (i%2==0) {
+			return true;
+		}else {
+		return false;
+		}
+	}
+	
+	
 }
