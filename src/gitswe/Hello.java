0c57315ef00b52f0902a370ddea582c0fc147e6a
DESKTOP-T4K3K5I\Jo_PC <jo_sometime@hotmail.com>
2017-09-14 08:20:41 +0700

    Hello P'Tong

--- src/gitswe/Hello.java
+++ src/gitswe/Hello.java
@@ -5,7 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
         System.out.println("Hello Java Github");
         System.out.println("Hello Java Github 2 ");
-        System.out.println("Hello Java Github 3 ")
+        System.out.println("Hello Java Github 3 ");
+        System.out.println("Hello P'Tong ");
     }
 
 
