dd971576aa684f04578e06f48635aeb1d3eb3bdd
THASAWAT\Thasawat <rthasawat@gmaill.com>
2017-09-18 14:49:58 +0700

    grade อิอิ

---
+++ src/gitswe/Grade.java
@@ -0,0 +1,34 @@
+package gitswe;
+
+import java.util.Scanner;
+
+public class Grade {
+	static Scanner t = new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Enter Grade : ");
+		int score = t.nextInt();
+		final String grade;  
+					if (score >= 80) {
+			           grade = "Grade:A";
+			        } else if (score >= 75) {
+			           grade = "Grade:B";
+			        } else if (score >= 75) {
+			            grade = "Grade:B";
+			        } else if (score >= 65) {
+			           grade = "Grade:C+";
+			        } else if (score >= 60) {
+				           grade = "Grade:C";
+			        } else if (score >= 55) {
+				           grade = "Grade:D+";
+			        } else if (score >= 50) {
+				           grade = "Grade:D";
+			        } else {
+			            grade = "Grade:F";
+			     }
+		System.out.printf (grade);
+	}
+
+}
+
