1b0b911b86a1cac0de625bfcfef1b6e24a12c46a
DESKTOP-T4K3K5I\Jo_PC <jo_sometime@hotmail.com>
2017-09-18 14:40:14 +0700

    input 100 num

--- src/gitswe/masterv2.java
+++ src/gitswe/masterv2.java
@@ -1,5 +1,18 @@
 package gitswe;
 
-public class masterv2 {
+import java.util.Scanner;
 
+public class masterv2 {
+	public static void main(String[] args) {
+		
+	//	System.out.print("Enter number to check odd or even : ");
+        Scanner input = new Scanner(System.in);
+        int[] x = new int[100];
+        for (int i = 0 ;i < 100; i++) {
+        	x[i]=i;
+        	System.out.println(x[i]);
+        }
+        	
+       
+    }
 }
