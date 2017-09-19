1027eaa2bdc78e1a525150411e07835b96fb7928
THASAWAT\Thasawat <rthasawat@gmaill.com>
2017-09-19 10:41:55 +0700

    double

--- src/Salary/calCulate.java
+++ src/Salary/calCulate.java
@@ -1,10 +1,13 @@
 package Salary;
 import java.util.Scanner;
 public class calCulate {
-
+	static Scanner t = new Scanner(System.in);
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		double sale = t.nextDouble();
+		double salary = t.nextDouble();
+		double x = t.nextDouble();
+        Scanner input = new Scanner(System.in);
 		 if(sale<=25000) { 
 			 
 			 x=sale*(1/100);
@@ -20,5 +23,5 @@ public class calCulate {
 			 salary = salary+x;	        
 		 }
 	}
-
+	
 }
