71ec66424315d0820f6103fbfdd707130aacbeb1
DESKTOP-T4K3K5I\Jo_PC <jo_sometime@hotmail.com>
2017-09-19 10:59:34 +0700

    insert V2

--- src/Salary/inSert.java
+++ src/Salary/inSert.java
@@ -4,21 +4,25 @@ import java.util.Scanner;
 
 public class inSert {
 
-	public static void main(String[] args) {
+	public inSert(){
 		// TODO Auto-generated method stub
 		String id;
 		String name;
 		Double salary;
+		Double sale;
 		
 		Scanner sc = new Scanner(System.in);
 		id = sc.nextLine();
 		name = sc.nextLine();
 		salary = sc.nextDouble();
+		sale=sc.nextDouble();
+	
 		
 		System.out.println(id);
 		System.out.println(name);	
 		System.out.println(salary);
+		System.out.println(sale);
+		
 	}
-
 }
 
