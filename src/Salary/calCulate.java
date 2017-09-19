f039c9df5fe397893461ccfe6477da0f4f33559f
THASAWAT\Thasawat <rthasawat@gmaill.com>
2017-09-19 10:33:28 +0700

    cal

--- src/Salary/calCulate.java
+++ src/Salary/calCulate.java
@@ -1,10 +1,22 @@
 package Salary;
-
+import java.util.Scanner;
 public class calCulate {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		 if(sale<=25000) { 	
+			 x=sale*(1/100);
+			 salary = salary+x;
+		        
+		 }else if(sale>=25001 && sale<=50000) {
+		        
+			 x=sale*(2/100);
+			 salary = salary+x;		        
+		 }else {
+			 x=sale*(3/100);
+			 salary = salary+x;	        
+		 }
 	}
 
 }
