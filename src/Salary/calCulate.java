e1e407098b101c19741d40914540722ee37ab161
THASAWAT\Thasawat <rthasawat@gmaill.com>
2017-09-19 11:07:53 +0700

    edit cal

--- src/Salary/calCulate.java
+++ src/Salary/calCulate.java
@@ -1,38 +1,11 @@
 package Salary;
 import java.util.Scanner;
 public class calCulate {
-<<<<<<< HEAD
-	static Scanner t = new Scanner(System.in);
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		double sale = t.nextDouble();
-		double salary = t.nextDouble();
-		double x = t.nextDouble();
-        Scanner input = new Scanner(System.in);
-		 if(sale<=25000) { 
-			 
-			 x=sale*(1/100);
-			 salary = salary+x;
-		        
-		 }else if(sale>=25001 && sale<=50000) {
-=======
-
 String id;
 String name;
 Double salary;
 Double sale;
->>>>>>> refs/heads/calCulate
 
-<<<<<<< HEAD
-			 x=sale*(2/100);
-			 salary = salary+x;		        
-		 }else {
-			 
-			 x=sale*(3/100);
-			 salary = salary+x;	        
-		 }
-	}
-=======
 static Scanner t = new Scanner(System.in);
 public calCulate(String insert, String name , Double salary ,Double sale ) {
 	// TODO Auto-generated constructor stub
@@ -56,6 +29,4 @@ public calCulate(String insert, String name , Double salary ,Double sale ) {
 		 salary = salary+x;	        
 	 }
 }
->>>>>>> refs/heads/calCulate
-	
 }
